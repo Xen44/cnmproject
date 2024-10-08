67b4030d5fc4c263a3e96c961ca83cdf5a2704ae
Xen44 <hyukimat@gmail.com>
2024-10-09 02:55:22 +0700

    initial

---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\Admin\DashboardController;
+use  App\Http\Controllers\Admin\SettingController;
+use  App\Http\Controllers\Admin\RoleController;
+use  App\Http\Controllers\LangController;
+use  App\Http\Controllers\Admin\UserController;
+use  App\Http\Controllers\Admin\VillageDataController;
+use  App\Http\Controllers\Admin\DistrictDataController;
+use App\Http\Controllers\Admin\KhomDataController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(
+        ['namespace' => 'Admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
+            Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+            Route::get('/setting/user', [UserController::class, 'index'])->name('setting.user');
+            Route::get('/user/get-user', [UserController::class, 'getUser'])->name('user.get-user');
+            Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+            Route::post('/user/destroy', [UserController::class, 'destroy'])->name('user.destroy')->middleware('permission:users-read');
+
+            Route::get('/setting/role', [RoleController::class, 'index'])->name('role');
+            Route::get('/setting/get-role', [RoleController::class, 'getRole'])->name('setting.get-role');
+            Route::post('/role-permission/store', [RoleController::class, 'store'])->name('role-permission.store');
+            Route::post('/role/store', [RoleController::class, 'storeRole'])->name('role.store');
+            Route::post('/role/destroy', [RoleController::class, 'destroyRole'])->name('role.destroy');
+
+            Route::get('/village-data', [VillageDataController::class, 'index'])->name('village-data');
+            Route::get('/village-data/get-attributes', [VillageDataController::class, 'getAttributes'])->name('village-data.get-attributes');
+            Route::get('/village-data/get-data/{id}', [VillageDataController::class, 'getData'])->name('village-data.get-data');
+
+            // Route::get('/khom-data', [KhomDataController::class, 'index'])->name('khom-data');
+            // Route::get('/khom-data/get-attributes', [KhomDataController::class, 'getAttributes'])->name('khom-data.get-attributes');
+            // Route::get('/khom-data/get-data/{id}', [KhomDataController::class, 'getData'])->name('khom-data.get-data');
+
+            Route::any('/khom-data', [KhomDataController::class, 'index'])->name('khom-data');
+            Route::any('/khom-data/getdata', [KhomDataController::class, 'getdata']);
+            Route::any('/khom-data/getdetail', [KhomDataController::class, 'getdetail']);
+            Route::post('/khom-data/save', [KhomDataController::class, 'save']);
+            Route::post('/khom-data/delete', [KhomDataController::class, 'delete']);
+
+            Route::any('/district-data', [DistrictDataController::class, 'index'])->name('district-data');
+            Route::any('/district-data/getdata', [DistrictDataController::class, 'getdata']);
+            Route::any('/district-data/getdetail', [DistrictDataController::class, 'getdetail']);
+            Route::post('/district-data/save', [DistrictDataController::class, 'save']);
+            Route::post('/district-data/delete', [DistrictDataController::class, 'delete']);
+        }
+    );
+});
+
+Auth::routes();
+Route::get('lang/change', [LangController::class, 'change'])->name('changeLang');
